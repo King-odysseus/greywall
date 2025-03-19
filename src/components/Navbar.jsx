18ec7ba836fab6e4b1a752e315301d6d39d94b13
Nate <96822493+King-odysseus@users.noreply.github.com>
2025-03-19 15:10:38 +0000

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/contact">
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/services">
+            Services
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
